28a966a70d83ef55b740f496fc5ac280c6f07ac5
El Senpai <El Senpai@EL-SENPAI>
2021-05-22 23:50:45 +0300

    Lom

--- northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
+++ northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -7,10 +7,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 @Entity
 @Table(name="Products")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class Product {
 		    @Id
@@ -34,15 +38,5 @@ public class Product {
 		    @Column(name="quantity_per_unit")	
 		    private String quantityPerUnit;
 
-		    public Product() {}
-		    public Product(int id, int categoryId, String productName, double unitPrice, short unitsInStock,
-		    String quantityPerUnit) {
-		    	super();
-		    	this.id = id;
-		    	this.categoryId = categoryId;
-		    	this.productName = productName;
-		    	this.unitPrice = unitPrice;
-		    	this.unitsInStock = unitsInStock;
-		    	this.quantityPerUnit = quantityPerUnit;
-		    }
+		    
 }
