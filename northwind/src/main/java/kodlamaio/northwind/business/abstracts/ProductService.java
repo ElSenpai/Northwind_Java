5b0b3a931be00766f20c13f2441d993e9442b9c1
El Senpai <El Senpai@EL-SENPAI>
2021-05-22 23:06:57 +0300

    DataResult on service

--- northwind/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
+++ northwind/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
@@ -2,9 +2,11 @@ package kodlamaio.northwind.business.abstracts;
 
 import java.util.List;
 
+import kodlamaio.northwind.core.utilities.results.DataResult;
 import kodlamaio.northwind.entities.concretes.Product;
 
 public interface ProductService {
     
-	    List<Product> getAll();
+	   // <Product> getAll();
+	    DataResult<List<Product>> getAll();
 }
--- northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
+++ northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.SuccessDataResult;
 import kodlamaio.northwind.dataAccess.abstracts.ProductDao;
 import kodlamaio.northwind.entities.concretes.Product;
 
@@ -21,9 +23,10 @@ public class ProductManager implements ProductService {
 	}
 
 	@Override
-	public List<Product> getAll() {
+	public DataResult<List<Product>> getAll() {
 		// TODO Auto-generated method stub
-		return this.productDao.findAll();
+		return new SuccessDataResult<List<Product>>(this.productDao.findAll(),"Data Listelendi");
+				
 	}
 
 }
