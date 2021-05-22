98d86bb82eb97ea8bf6d7339d669b2b5318eee7c
El Senpai <El Senpai@EL-SENPAI>
2021-05-22 23:42:28 +0300

    entities

--- northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
+++ northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -3,6 +3,7 @@ package kodlamaio.northwind.entities.concretes;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -13,7 +14,7 @@ import lombok.Data;
 
 public class Product {
 		    @Id
-			@GeneratedValue
+			@GeneratedValue(strategy=GenerationType.IDENTITY)
 			@Column(name="product_id")	
 		    private int id;
 
