dcd1865ec0d5ad84984312c83b018ceae713b05e
El Senpai <El Senpai@EL-SENPAI>
2021-05-27 22:10:30 +0300

    service

--- northwind/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
+++ northwind/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
@@ -2,7 +2,7 @@ package kodlamaio.northwind.business.abstracts;
 
 import java.util.List;
 
-
+import org.springframework.data.jpa.repository.Query;
 
 import kodlamaio.northwind.core.utilities.results.DataResult;
 import kodlamaio.northwind.core.utilities.results.Result;
@@ -13,5 +13,19 @@ public interface ProductService {
 	   // <Product> getAll();
 	    DataResult<List<Product>> getAll();
 	    Result add(Product product);
+	    
+	    DataResult <Product> getByProductName(String productName);
+		
+	    DataResult<Product> getByProductNameAndCategoryId(String productName,int categoryId);
+		
+		DataResult<List<Product>> getByProductNameOrCategoryId(String productName,int categoryId);
+		
+		DataResult<List<Product>> getByCategoryIdIn(List<Integer> categories);
+		
+		DataResult<List<Product>> getByProductNameContains(String productName);
+		
+		DataResult<List<Product>> getByProductNameStartsWith(String productName);
+		
+		DataResult<List<Product>> getByNameAndCategory(String productName,int categoryId);
 	    
 }
