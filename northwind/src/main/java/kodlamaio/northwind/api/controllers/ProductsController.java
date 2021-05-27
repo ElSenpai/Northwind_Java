10fab1dc72a602978de6913ce68ce90c685303b3
El Senpai <El Senpai@EL-SENPAI>
2021-05-27 22:26:08 +0300

    paramsız yiyor aslında

--- northwind/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
+++ northwind/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.northwind.business.abstracts.ProductService;
@@ -40,7 +41,7 @@ public class ProductsController {
 	}
 	
 	@GetMapping("/getByProductName")
-	public DataResult<Product> getByProductName(String productName){
+	public DataResult<Product> getByProductName(@RequestParam String productName){
 		return this.productService.getByProductName(productName);
 	}
 	//
