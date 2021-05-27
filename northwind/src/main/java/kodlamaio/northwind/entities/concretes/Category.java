d1d4b8d504b98abd7818aa6fd7f477c24265586c
El Senpai <El Senpai@EL-SENPAI>
2021-05-27 21:22:20 +0300

    one to many

--- northwind/src/main/java/kodlamaio/northwind/entities/concretes/Category.java
+++ northwind/src/main/java/kodlamaio/northwind/entities/concretes/Category.java
@@ -1,7 +1,10 @@
 package kodlamaio.northwind.entities.concretes;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -21,4 +24,7 @@ public class Category {
 	
 	@Column(name="category_name")
 	private String categoryName;
+	
+	@OneToMany(mappedBy="categories")
+	private List<Product> products;
 }
