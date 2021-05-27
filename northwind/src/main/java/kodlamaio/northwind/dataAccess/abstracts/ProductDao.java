4a0c23fb7a1a0afd21a7c4c55950bc420cf350df
El Senpai <El Senpai@EL-SENPAI>
2021-05-27 21:39:58 +0300

    hey

--- northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
+++ northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
@@ -1,5 +1,7 @@
 package kodlamaio.northwind.dataAccess.abstracts;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import kodlamaio.northwind.entities.concretes.Product;
@@ -7,4 +9,10 @@ import kodlamaio.northwind.entities.concretes.Product;
 public interface ProductDao extends JpaRepository<Product,Integer> {
 		
 	Product getByProductName(String productName);
+	
+	Product getByProductNameAndCategoryId(String productName,int categoryId);
+	
+	List<Product> getByProductNameOrCategoryId(String productName,int categoryId);
+	
+	List<Product> getByCategoryIdIn(List<Integer> categories);
 }
