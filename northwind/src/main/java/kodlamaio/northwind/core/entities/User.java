5abaeb4222cd9bef6fd1b59cbde610d267a9ec20
El Senpai <El Senpai@EL-SENPAI>
2021-05-30 23:11:35 +0300

    entities.user on core

---
+++ northwind/src/main/java/kodlamaio/northwind/core/entities/User.java
@@ -0,0 +1,27 @@
+package kodlamaio.northwind.core.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="password")
+	private String password;
+}
