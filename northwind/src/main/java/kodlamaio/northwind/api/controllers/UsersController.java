f1f2b9598f0c34f17882c605587b64ae7a2f938c
El Senpai <El Senpai@EL-SENPAI>
2021-05-31 00:08:24 +0300

    hey

--- northwind/src/main/java/kodlamaio/northwind/api/controllers/UsersController.java
+++ northwind/src/main/java/kodlamaio/northwind/api/controllers/UsersController.java
@@ -1,7 +1,12 @@
 package kodlamaio.northwind.api.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,7 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.northwind.business.abstracts.UserService;
 import kodlamaio.northwind.core.entities.User;
-import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.ErrorDataResult;
+
 
 
 @RestController
@@ -31,7 +37,16 @@ public class UsersController {
 		
 		return ResponseEntity.ok(this.userService.add(user));
 	}
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ErrorDataResult<Object> handleValidationException(){
+		Map<String, String> validationErrors=new HashMap<String,String>();
+		
+	}
 	
 	
+//	@GetMapping("/getbyemail")
+//	public ResponseEntity<?> findByEmail(String email) {
+//		return this.userService.findByEmail(email);
+//	}
 	
 }
