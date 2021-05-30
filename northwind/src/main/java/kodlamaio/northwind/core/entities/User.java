d69b10fa6d28f03bf97ec98eccbb3d031c3ebbfe
El Senpai <El Senpai@EL-SENPAI>
2021-05-30 23:13:34 +0300

    hey

--- northwind/src/main/java/kodlamaio/northwind/core/entities/User.java
+++ northwind/src/main/java/kodlamaio/northwind/core/entities/User.java
@@ -7,11 +7,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @Table(name="users")
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
 	@Id
